8193e9160cffee522dd9b3ff6c48d30e64e4066b
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-01-17 19:14:06 -0500

    adding code

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	fmt.Println("Hello everyone, this is my first golang program!")
+	foo()
+	fmt.Println("here is some more intro")
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("I am an extra function")
 }
