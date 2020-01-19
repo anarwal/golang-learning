ad600729ebf737ea172ffc9291b09d8703c90e63
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-01-19 15:31:06 -0500

    jedi-level-1, exercise-2 solution

---
+++ jedi-level-01/exercise-02/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// Declare three variables using var and provide them package level scope. Do not assign any value to these variables. Provide following type to each variable: x type int, y type string and z type bool.
+// in main func print out the value of each identifier
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
