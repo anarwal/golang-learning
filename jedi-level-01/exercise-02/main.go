e60394dd87590a6f258471bd610b1c0aa53f5bc8
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-01-19 15:53:03 -0500

    formatting files

--- jedi-level-01/exercise-02/main.go
+++ jedi-level-01/exercise-02/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 // Declare three variables using var and provide them package level scope. Do not assign any value to these variables. Provide following type to each variable: x type int, y type string and z type bool.
 // in main func print out the value of each identifier
+
 var x int
 var y string
 var z bool
