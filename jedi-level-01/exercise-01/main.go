7d2970ddac058107b0d4001b7cfd1aa82db85f34
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-01-19 15:26:32 -0500

    adding problem statement to jedi-level-1, exercise-1 solution

--- jedi-level-01/exercise-01/main.go
+++ jedi-level-01/exercise-01/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// assign variable x as 42, y as "james Bond" and z as true using short declaration operator
+// print the variables first as single print statement and then as multiple print statement.
+
 func main() {
 	x := 42
 	y := "James Bond"
