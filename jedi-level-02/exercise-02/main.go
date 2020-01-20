83f8fc5049723a9f9f17363a2af7a885db475438
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-01-19 21:17:57 -0500

    jedi-level-2, exercise-2 solution

---
+++ jedi-level-02/exercise-02/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// Using the following operators, write expressions and assign their values to variables: ==, <=, >=, != , <, >
+
+func main() {
+	a := 42 == 42
+	b := 42 <= 43
+	c := 43 >= 42
+	d := 42 != 42
+	e := 42 < 43
+	f := 43 > 42
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+}
