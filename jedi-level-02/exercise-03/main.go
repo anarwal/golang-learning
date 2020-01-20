98b6589baa306a211915f149fdb351edbb759159
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-01-19 21:19:57 -0500

    jedi-level-2, exercise-3 solution

---
+++ jedi-level-02/exercise-03/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// Create TYPED and UNTYPED constants. Print the values of the constants
+
+const (
+	a int = 42
+	b     = "James Bond"
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+}
