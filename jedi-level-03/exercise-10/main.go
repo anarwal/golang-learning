c7fe465236552fe8801d571b872766930d6360cf
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-01-20 15:01:29 -0500

    jedi-level-3, exercise-10 solution

---
+++ jedi-level-03/exercise-10/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Write down what these print: fmt.Println(true && true); fmt.Println(true && false); fmt.Println(true || true); fmt.Println(true || false); fmt.Println(!true)
+
+func main() {
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
