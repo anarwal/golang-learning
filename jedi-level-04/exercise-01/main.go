86d5dd6a61d8b3708b01b6c2fc17c5344b5d4f32
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-01-21 22:54:49 -0500

    jedi-level-4, exercise-1 solution, migrating to use array instead of slice

--- jedi-level-04/exercise-01/main.go
+++ jedi-level-04/exercise-01/main.go
@@ -5,7 +5,7 @@ import "fmt"
 // Using a COMPOSITE LITERAL: create an ARRAY which holds 5 VALUES of TYPE int; assign VALUES to each index position; Range over the array and print the values out; Using format printing; print out the TYPE of the array
 
 func main() {
-	a := []int{3, 4, 5, 6, 7}
+	a := [5]int{3, 4, 5, 6, 7}
 
 	for i, v := range a {
 		fmt.Println(i, v)
