5e3ca68e81f8857ffadfa960b196bba5546bafeb
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-01-24 18:38:48 -0500

    jedi-level-4, exercise-9 solution

---
+++ jedi-level-04/exercise-09/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// Using the code from the exercise jedi-level09/exercise-08, add a record to your map. Now print the map out using the “range” loop
+
+func main() {
+	m := make(map[string][]string)
+
+	m["bond_james"] = []string{`Shaken, not stirred`, `Martinis`, `Women`}
+	m["moneypenny_miss"] = []string{`James Bond`, `Literature`, `Computer Science`}
+	m["no_dr"] = []string{`Being evil`, `Ice cream`, `Sunsets`}
+
+	for i, v := range m {
+		fmt.Println("Key: ", i)
+		for j, val := range v {
+			fmt.Printf("%v : %v\n", j, val)
+		}
+	}
+	// adding new value
+	m["new_mr_bond"] = []string{`not smart`, `too lazy`}
+
+	for i, v := range m {
+		fmt.Println("Key: ", i)
+		for j, val := range v {
+			fmt.Printf("%v : %v\n", j, val)
+		}
+	}
+}
