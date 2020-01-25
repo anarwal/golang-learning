fe435a81082f6f63e46e6f1d11ce22b633517da5
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-01-24 19:18:25 -0500

    update jedi-level-5, exercise-1 solution

---
+++ jedi-level-05/exercise-01/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// Create your own type “person” which will have an underlying type of “struct” so that it can store the following data:
+//first name
+//last name
+//favorite ice cream flavors
+//Create two VALUES of TYPE person. Print out the values, ranging over the elements in the slice which stores the favorite flavors.
+
+type person struct {
+	firstName         string
+	lastName          string
+	favIceCreamFlavor []string
+}
+
+func main() {
+	p1 := person{}
+	p1.firstName = "john"
+	p1.lastName = "doe"
+	p1.favIceCreamFlavor = []string{"vanilla", "chocolate"}
+
+	p2 := person{"sam", "doe", []string{"chocolate", "black-current"}}
+
+	fmt.Println(p1)
+	fmt.Println(p2)
+
+	for i, v := range p1.favIceCreamFlavor {
+		fmt.Println(i, v)
+	}
+
+	for i, v := range p2.favIceCreamFlavor {
+		fmt.Println(i, v)
+	}
+
+}
