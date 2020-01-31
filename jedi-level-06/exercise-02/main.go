7436ccdfbeb9c1519edb54662062f09b19ba232b
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-01-30 20:25:58 -0500

    jedi-level-06, exercise-02 solution

---
+++ jedi-level-06/exercise-02/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// create a func with the identifier foo that
+//		- takes in a variadic parameter of type int
+//		- pass in a value of type []int into your func (unfurl the []int)
+//		- returns the sum of all values of type int passed in
+//create a func with the identifier bar that
+//		- takes in a parameter of type []int
+//		- returns the sum of all values of type int passed in
+
+func main() {
+	x := foo(1, 2, 3, 4, 5)
+	fmt.Println("Value of sum from foo is: ", x)
+
+	y := []int{1, 2, 3, 4}
+	fmt.Println("Value of sum from bar is: ", bar(y))
+
+}
+
+func foo(x ...int) int {
+	sum := 0
+	for _, v := range x {
+		sum += v
+	}
+	return sum
+}
+
+func bar(x []int) int {
+	sum := 0
+	for _, v := range x {
+		sum += v
+	}
+	return sum
+}
