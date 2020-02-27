93862bdac7f5dc9b0a7a1951829b2da7bc56b46b
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-02-26 19:55:50 -0500

    jedi-level-08, exercise-05 solution

---
+++ jedi-level-08/exercise-05/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// Sort the []user by
+//   - age
+//   - last
+// also sort each []string "Sayings" for each user
+// Print everything out using the range clause
+
+type user struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
+}
+
+type ByAge []user
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
+type ByLast []user
+
+func (l ByLast) Len() int           { return len(l) }
+func (l ByLast) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l ByLast) Less(i, j int) bool { return l[i].Last < l[j].Last }
+
+func main() {
+	u1 := user{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is not guarantee of innovation",
+		},
+	}
+	u2 := user{
+		First: "M",
+		Last:  "B",
+		Age:   50,
+		Sayings: []string{
+			"Oh James, you killed him",
+			"Dear James",
+		},
+	}
+
+	u3 := user{
+		First: "Bob",
+		Last:  "Call",
+		Age:   22,
+		Sayings: []string{
+			"Bob, its me",
+			"Dear M, its Bob",
+		},
+	}
+
+	users := []user{u1, u2, u3}
+	fmt.Println(users)
+	fmt.Println("------")
+	sort.Sort(ByAge(users))
+	fmt.Println(users)
+	fmt.Println("------")
+	sort.Sort(ByLast(users))
+}
