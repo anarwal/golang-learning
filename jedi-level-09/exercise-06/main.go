846967d98399e054133388ee85750e1da82337ca
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-02-27 16:06:41 -0500

    jedi-level-09, exercise-06 solution

---
+++ jedi-level-09/exercise-06/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// Create a program that prints out your OS and ARCH. Use the following commands to run it
+//   - go run
+//   - go build
+//   - go install
+
+func main() {
+	fmt.Println("OS", runtime.GOOS)
+	fmt.Println("ARCH", runtime.GOARCH)
+}
