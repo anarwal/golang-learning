2894eb179e6a56d6cc1bfc37ae7019a37a8c1f1e
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-02-28 20:06:46 -0500

    jedi-level-10, exercise-05 solution

---
+++ jedi-level-10/exercise-05/main.go
@@ -0,0 +1,23 @@
+package main
+
+//Show the comma ok idiom starting with https://play.golang.org/p/YHOMV9NYKK
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := make(chan int)
+
+	go func() {
+		c <- 42
+	}()
+
+	v, ok := <-c
+	fmt.Println(v, ok)
+
+	close(c)
+
+	v, ok = <-c
+	fmt.Println(v, ok)
+}
